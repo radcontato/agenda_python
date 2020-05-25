d7737a3b576293472b251500b34ed3c69316a287
Roberto Albano <rad.contato@gmail.com>
2020-05-25 18:11:59 -0300

    incluido função e tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,5 +5,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', views.index)
+    path('', views.index),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/',views.logout_user)
 ]
