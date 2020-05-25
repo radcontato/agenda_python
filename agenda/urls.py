3da1e251abc93f5432054d0549f9d00d51838c2f
Roberto Albano <rad.contato@gmail.com>
2020-05-25 18:49:53 -0300

    Incluindo template para inserção de eventos na agenda.

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,6 +5,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+    path('agenda/evento/', views.evento),    
+    path('agenda/evento/submit', views.submit_evento),
     path('', views.index),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
