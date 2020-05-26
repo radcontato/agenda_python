f3a5c9c6f9a5fac4c1fadf8d4e2cfdf6fea2100d
Roberto Albano <rad.contato@gmail.com>
2020-05-26 20:23:44 -0300

    Adicionado retorno de lista por json, retorno de http404 e config timezone

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')poqn&hdnn0qf6sab=qerg)9sczxmao7u@rg8odg2ezn-qn!#4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- agenda/urls.py
+++ agenda/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', views.index),
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('agenda/evento/', views.evento),    
+    path('agenda/lista/<int:id_usuario>/', views.json_lista_evento),
+    path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
-    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),    
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
+    path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
-    path('logout/',views.logout_user)
+    path('logout/', views.logout_user)
 ]
